884a403c651189f6e0e71959368a264bd485067a
Vitaliy <ist70@mail.ru>
2016-01-27 20:03:35 +0400

    Модель News, метод findLastNews

--- lesson1/index.php
+++ lesson1/index.php
@@ -3,6 +3,4 @@ require __DIR__ . '/autoload.php';
 $users = \App\Models\User::findById(2);
 
 $news = \App\Models\News::findLastNews(6);
-?> <pre><?php
-    var_dump($news);?>
-    </pre>
+include __DIR__ . '/';
