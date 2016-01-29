48fc2dcde0348d63a55251769c3cfc8a653a067f
Vitaliy <ist70@mail.ru>
2016-01-29 12:40:14 +0400

    Исправление ошибок

--- lesson1/Tests2/tests1.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/autoload.php';
-
-$test = new \App\Db();
-$result1 = $test->execute('SELECT * FROM users');
-$result2 = $test->execute('SELECT * FROM users WHERE id_user = :id', [':id' => 1]);
