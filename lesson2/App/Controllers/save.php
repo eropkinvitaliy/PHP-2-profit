5ce936b562b818ff784ebe679e5712da1bbb15a2
Vitaliy <ist70@mail.ru>
2016-02-02 12:24:31 +0400

    TSingltone добавил как трейт в Config и Db

--- lesson2/App/Controllers/save.php
+++ lesson2/App/Controllers/save.php
@@ -5,8 +5,8 @@ require __DIR__ . '/../../autoload.php';
 $post = $_POST;
 if (!empty($post)) {
     $article = new News();
-    $article->title = $post['title'];
-    $article->description = $post['description'];
+    $article->title = trim($post['title']);
+    $article->description = trim($post['description']);
     $article->save();
     include __DIR__ . '/../../Views/article.php';
 } else {
