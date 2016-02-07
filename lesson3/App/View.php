e0ea93027e41594ad94f30318e99b6882b3c0751
Vitaliy <ist70@mail.ru>
2016-02-07 12:50:02 +0400

    Добавил модель View и шаблон

---
+++ lesson3/App/View.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+
+class View
+    implements \Countable
+{
+    protected $data = [];
+    public function __set($k, $v)
+    {
+        $this->data[$k] = $v;
+    }
+    public function __get($k)
+    {
+        return $this->data[$k];
+    }
+    public function render($template)
+    {
+        ob_start();
+        foreach ($this->data as $prop => $value) {
+            $$prop = $value;
+        }
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+    public function display($template)
+    {
+        echo $this->render($template);
+    }
+    /**
+     * Count elements of an object
+     * @return int The custom count as an integer.
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+}
--- lesson3/index.php
+++ lesson3/index.php
@@ -1,7 +1,11 @@
 <?php
 use App\Models\User;
 use App\Models\News;
+use App\View;
 require __DIR__ . '/autoload.php';
 
-$news = News::findLastRecords(3);
-include __DIR__ . '/Views/news.php';
+
+$view = new View();
+$view->title = 'Урок 3';
+$view->news = News::findLastRecords(3);
+$view->display(__DIR__ . '/App/templates/index.php');
